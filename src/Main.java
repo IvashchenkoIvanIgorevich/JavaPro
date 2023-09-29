84dec85ad72a523077ff59db5356512bdba7067f
Ivan Ivashchenko <ivachko2009@gmail.com>
2023-09-29 11:22:52 +0300

    L-2 Create two classes with the same name

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
                 "email@email.com", "12345678", 30);
 
         System.out.println(engineer);
+
+        client.SameName clientSameName = new client.SameName();
+        server.SameName serverSameName = new server.SameName();
     }
 }
