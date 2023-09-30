26e888c1476f2373ce848f02daa1553fbcaed79f
Ivan Ivashchenko <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2023-09-30 15:03:22 +0300

    L-2 OOP introduction (#2)
    * L-2 Create Employee class
    
    * L-2 Create two classes with the same name
    
    * L-2 Create Car class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Employee engineer = new Employee("Name", "LastName", "Surname", Employee.Position.ENGINEER,
+                "email@email.com", "12345678", 30);
+
+        System.out.println(engineer);
+
+        client.SameName clientSameName = new client.SameName();
+        server.SameName serverSameName = new server.SameName();
     }
 }
