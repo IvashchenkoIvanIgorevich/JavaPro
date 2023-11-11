edc61877ddee9020964ac9528276b570c62a7ed2
Ivan Ivashchenko <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2023-11-11 11:42:22 +0200

    L-11 Practising with exception (#11)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import homework.practiceexception.ArrayValueCalculator;
+
 public class Main {
 
     public static void main(String[] args) {
+        String[][] input = {
+                {"1","2","3","4"},
+                {"5","6","7","8"},
+                {"9","10","11","12"},
+                {"13","14","15", "+"}
+        };
 
+        try {
+            int sum = ArrayValueCalculator.doCalc(input);
+            System.out.println(sum);
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
     }
 }
