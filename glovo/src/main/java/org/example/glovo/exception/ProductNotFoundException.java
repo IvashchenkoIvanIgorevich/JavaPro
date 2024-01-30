63c9b7270452a28f9bd232cbdb80f47cbc30ab44
Ivan Ivashchenko <62796151+IvashchenkoIvanIgorevich@users.noreply.github.com>
2024-01-30 15:29:40 +0200

    Revert "HW-21 Glovo spring appp DB storage"

--- glovo/src/main/java/org/example/glovo/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.example.glovo.exception;
-
-public class ProductNotFoundException extends Exception {
-
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
--- glovo/src/main/java/org/example/glovo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.glovo.repository;
-
-import org.example.glovo.entity.ProductEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
-}
