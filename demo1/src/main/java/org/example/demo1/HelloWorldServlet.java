78a3630a54f6d87f278056bd2727260ad0eea603
Ivan Ivashchenko <ivachko2009@gmail.com>
2024-02-08 15:18:22 +0200

    HW-22 Create hello world servlet

---
+++ demo1/src/main/java/org/example/demo1/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package org.example.demo1;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.println("Hello World1213!");
+    }
+}
